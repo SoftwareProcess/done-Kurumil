17da6f7c08d0794101b339068e7ca648da8ce63a
jacks <jacks@Lee-Alienware>
2021-08-29 19:45:07 -0500

    assignment 2 complete

--- Assignment/collapse.py
+++ Assignment/collapse.py
@@ -1,2 +1,19 @@
-def  collapse(value):
-    return '5'
+def collapse(value):   
+    total = 0
+    remainingDigits = len(value)
+    
+    if isinstance(value,str) and value.isnumeric() and remainingDigits < 51:
+    
+        while remainingDigits == 1:
+            return value
+    
+        while remainingDigits > 1:
+            total = 0
+            for i in value:
+                total += int(i)
+                value = str(total)
+                remainingDigits = len(str(total))
+    
+        return value
+    else:
+        return None
--- Assignment/collapseTest.py
+++ Assignment/collapseTest.py
@@ -1,13 +1,65 @@
 import unittest
 import Assignment.collapse as collapse
 
-
 class CollapseTest(unittest.TestCase):
 
-# Happy path test
-    def test100_100_ShouldCollapseSingleDigit(self):
+# Happy path:  nominal value within bounds
+    def test100_010_ShouldCollapseOneGoodDigit(self):
         value = '5'
         expectedResult = '5'
         actualResult = collapse.collapse(value)
         self.assertEqual(expectedResult, actualResult)
-    
+
+    def test_ShouldCollapseOneGoodDigit(self):
+        value = '0'
+        expectedResult = '0'
+        actualResult = collapse.collapse(value)
+        self.assertEqual(expectedResult, actualResult)
+        
+    def test_ShouldCollapseTwoGoodDigits(self):
+        value = '10'
+        expectedResult = '1'
+        actualResult = collapse.collapse(value)
+        self.assertEqual(expectedResult, actualResult)
+        
+    def test_ShouldCollapseThreeGoodDigits(self):
+        value = '123'
+        expectedResult = '6'
+        actualResult = collapse.collapse(value)
+        self.assertEqual(expectedResult, actualResult)
+        
+    def test_ShouldCollapseTwoSameGoodDigits(self):
+        value = '99'
+        expectedResult = '9'
+        actualResult = collapse.collapse(value)
+        self.assertEqual(expectedResult, actualResult)
+        
+    def test_ShouldCollapseFiveGoodDigits(self):
+        value = '98769'
+        expectedResult = '3'
+        actualResult = collapse.collapse(value)
+        self.assertEqual(expectedResult, actualResult)
+        
+    def test_ShouldNotCollapseZeroGoodDigits(self):
+        value = ''
+        expectedResult = None
+        actualResult = collapse.collapse(value)
+        self.assertEqual(expectedResult, actualResult)
+        
+    def test_ShouldNotCollapseOneBadDigits(self):
+        value = 'a'
+        expectedResult = None
+        actualResult = collapse.collapse(value)
+        self.assertEqual(expectedResult, actualResult)
+        
+    def test_ShouldNotCollapseValueBelowZero(self):
+        value = '-1'
+        expectedResult = None
+        actualResult = collapse.collapse(value)
+        self.assertEqual(expectedResult, actualResult)
+        
+    def test_ShouldNotCollapseWithoutParentheses(self):
+        value = 1
+        expectedResult = None
+        actualResult = collapse.collapse(value)
+        self.assertEqual(expectedResult, actualResult)
